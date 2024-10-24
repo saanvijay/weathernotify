56dfea5838a5e3495fb11fca0e665efd5636e200
Vijaya Prakash Masilamani <vijayaprakashmasilamani@Anandas-MacBook-Pro.local>
2024-10-23 23:33:12 -0400

    REST api boilerplate

--- weather-process/main.go
+++ weather-process/main.go
@@ -1,36 +1,25 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-
-	weathersubs "github.com/saanvijay/weathernotify/weather-subs"
+	"net/http"
 )
 
 func main() {
 
-	location, err := weathersubs.GetCurrentLocation()
+	Host := "localhost"
+	Port := "8080"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /getlocation", GetLocation)
+	mux.HandleFunc("GET /getforecast/{latitude}/{longitude}", GetForecast)
+
+	endpoint := fmt.Sprintf("%s:%s", Host, Port)
+	fmt.Printf("Server listening %s\n", endpoint)
+	err := http.ListenAndServe(endpoint, mux)
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
+		fmt.Printf("Error starting the server: %s\n", err)
 		return
 	}
-
-	fmt.Println(location)
-
-	grid, err := weathersubs.GetGridPoint(location)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return
-	}
-	fmt.Println(string(grid))
-
-	forecast, err := weathersubs.GetForeCast(location)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return
-	}
-	jsonData, _ := json.MarshalIndent(forecast.Properties.Periods, "", " ")
-
-	fmt.Println(string(jsonData))
 
 }
---
+++ weather-process/utils.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	weathersubs "github.com/saanvijay/weathernotify/weather-subs"
+)
+
+type Response struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+}
+
+func GetLocation(w http.ResponseWriter, r *http.Request) {
+	location, err := weathersubs.GetCurrentLocation()
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	res := Response{
+		Message: location,
+		Status:  200,
+	}
+	jsonData, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, "Error converting to JSON", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(location)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
+
+}
+
+func GetForecast(w http.ResponseWriter, r *http.Request) {
+	latitude := r.PathValue("latitude")
+	longitude := r.PathValue("longitude")
+	location := fmt.Sprintf("%s, %s", latitude, longitude)
+
+	fmt.Printf("location is %s\n", location)
+
+	forecast, err := weathersubs.GetForeCast(location)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	jsonData, _ := json.MarshalIndent(forecast.Properties.Periods, "", " ")
+
+	fmt.Println(string(jsonData))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
+}
